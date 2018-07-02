ff3a6ff443d8f8d3557cf0d3ecdf2db30f24a9ef
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-07-02 15:03:37 +0200

    First

--- wl/urls.py
+++ wl/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('enquiries.urls')),
+    url(r'^enquiries/', include('enquiries.urls')),
 ]
